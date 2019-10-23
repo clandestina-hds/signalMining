c1d936b64715d9dc6f3b5baecf9d8bae1897eb6d
clandestina-hds <patriciagonzalezgaspar@gmail.com>
2019-10-23 12:32:42 -0500

    Change class name

---
+++ MainWindow.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QKeySequence, QIcon
+from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QWidget, QTabWidget, QStatusBar, QMenuBar, QAction, QApplication, \
+    QFileDialog, QToolBar
+
+print("Hola")
